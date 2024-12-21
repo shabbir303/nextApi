db15ed95ba69de9d5ac581ce4a359fd6de99e324
shabbir <shabbirhossain660@gmail.com>
2024-12-21 14:13:16 +0600

    patch request

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,6 +1,8 @@
 import connect from "@/lib/db";
 import User from "@/lib/modals/user";
+import { Types } from "mongoose";
 import { NextResponse } from "next/server";
+const ObjectId = Types.ObjectId;
 
 export const GET = async () => {
   try {
@@ -35,9 +37,44 @@ export const POST = async (request: Request) => {
   } catch (err: unknown) {
     // Check if err is an instance of Error to safely access message
     const errorMessage = err instanceof Error ? err.message : "Unknown error";
-    return new NextResponse(
-      "Error creating user: " + errorMessage,
-      { status: 500 }
-    );
+    return new NextResponse("Error creating user: " + errorMessage, {
+      status: 500,
+    });
   }
 };
+
+export const PATCH = async (request: Request) => {
+  try {
+    const body = await request.json();
+    const { userId, newUserName } = body;
+    await connect();
+    if (!userId || !newUserName) {
+      return new NextResponse("id or newusername not found", { status: 404 });
+    }
+    if(!Types.ObjectId.isValid(userId)){
+        return new NextResponse("invalid userId", { status: 404 });
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+        {_id: new ObjectId(userId)},
+        { userName: newUserName }, 
+        { new: true });
+    
+        if(!updatedUser){
+            return new NextResponse("User not found", { status: 404 });
+        }
+        return new NextResponse(JSON.stringify({ message: "user updated", user: updatedUser }), { status: 200 });
+
+  } catch (err: unknown) {
+    // Check if err is an instance of Error to safely access message
+    const errorMessage = err instanceof Error ? err.message : "Unknown error";
+    return new NextResponse("Error creating user: " + errorMessage, {
+      status: 500,
+    });
+  }
+};
+
+
+export const DELETE = async(request:Request)=>{
+
+}
