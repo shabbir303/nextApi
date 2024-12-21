5e74955b5f56c393b80694bde062479a78893628
shabbir <shabbirhossain660@gmail.com>
2024-12-21 12:58:48 +0600

    post user request is successfull

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,23 +1,43 @@
-import connect from "@/lib/db"
+import connect from "@/lib/db";
 import User from "@/lib/modals/user";
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
+export const GET = async () => {
+  try {
+    await connect();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users));
+  } catch (error: unknown) {
+    let errorMessage = "Unknown error occurred";
 
-export const GET = async()=>{
-    try{
-        await connect();
-        const users = await User.find();
-        return new NextResponse(JSON.stringify(users));
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    } else if (typeof error === "string") {
+      errorMessage = error;
     }
-    catch (error: unknown) {
-        let errorMessage = "Unknown error occurred";
-        
-        if (error instanceof Error) {
-            errorMessage = error.message;
-        } else if (typeof error === "string") {
-            errorMessage = error;
-        }
-    
-        return new NextResponse("Error fetching data: " + errorMessage, { status: 500 });
-    }
-}
+
+    return new NextResponse("Error fetching data: " + errorMessage, {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    await connect();
+    const newUser = new User(body);
+    await newUser.save();
+    return new NextResponse(
+      JSON.stringify({ message: "user is created", user: newUser }),
+      { status: 200 }
+    );
+  } catch (err: unknown) {
+    // Check if err is an instance of Error to safely access message
+    const errorMessage = err instanceof Error ? err.message : "Unknown error";
+    return new NextResponse(
+      "Error creating user: " + errorMessage,
+      { status: 500 }
+    );
+  }
+};
