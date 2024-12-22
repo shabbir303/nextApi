4e5a36f95467f3638e711b23480deb53e3f4c9a2
shabbir <shabbirhossain660@gmail.com>
2024-12-22 12:56:58 +0600

    categories posted are created successfully

--- app/api/(dashboard)/categories/route.ts
+++ app/api/(dashboard)/categories/route.ts
@@ -34,8 +34,49 @@ export const GET = async(request:Request)=>{
     catch (err: unknown) {
             // Check if err is an instance of Error to safely access message
             const errorMessage = err instanceof Error ? err.message : "Unknown error";
-            return new NextResponse("Error creating user: " + errorMessage, {
+            return new NextResponse("Error fetching categories: " + errorMessage, {
               status: 500,
             });
           }
         }
+
+
+      export  const POST = async(request:Request)=>{
+        try{
+            const {searchParams} = new URL(request.url);
+            const userId = searchParams.get('userId');
+
+            const {title}= await request.json();
+
+            if(!userId || !Types.ObjectId.isValid(userId)){
+                return new NextResponse(JSON.stringify({message:"Invalid user ID"}), {
+                    status: 400,
+                });
+            }
+
+            await connect();
+
+            const user = await User.findById(userId);
+            if(!user){
+                return new NextResponse(JSON.stringify({message:"User not found"}), {
+                    status: 404,
+                });
+            }
+
+            const newCategory = new Category({
+                title,
+                user: new Types.ObjectId(userId)
+            });
+
+            await newCategory.save();
+            return new NextResponse(
+                JSON.stringify({message:"category is creatd",category:newCategory}), { status: 201 });
+        }
+        catch (err: unknown) {
+            // Check if err is an instance of Error to safely access message
+            const errorMessage = err instanceof Error ? err.message : "Unknown error";
+            return new NextResponse("Error creating categories: " + errorMessage, {
+              status: 500,
+            });
+          }
+      }
